645c54d31ce024de6d64acc91ffb6b1ffc4ffba5
Rafal Maciak <rafal.maciak@allegro.pl>
2021-12-03 22:03:09 +0100

    Days 1,2 and 3.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input
+        .asSequence()
+        .map { s -> s.toInt() }
+        .zipWithNext { a, b -> b - a }
+        .count { it > 0 }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+
+    fun part2(input: List<String>): Int = input
+        .asSequence()
+        .map { s -> s.toInt() }
+        .windowed(3, transform = { it.sum() })
+        .zipWithNext { a, b -> b - a }
+        .count { it > 0 }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
